aca9952ceb42380bed01dced5156c771a2568171
github username <github email address>
2020-02-07 16:14:01 -0500

    updating everything

---
+++ frontend/index.js
@@ -0,0 +1,10 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+  console.log('Photo App');
+
+  const photoUrl = 'http://localhost:3000/api/v1/photos';
+  // `${photoUrl}/${photo.id}`
+  // make sure to cd into backend to run rails s
+
+
+
+});
--- test/controllers/users_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class UsersControllerTest < ActionDispatch::IntegrationTest
-  test "should get name" do
-    get users_name_url
-    assert_response :success
-  end
-
-end
