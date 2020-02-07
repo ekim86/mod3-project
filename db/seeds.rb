1d44363b026cf4698373c5ce54ada0a94fd62af3
github username <github email address>
2020-02-07 15:45:30 -0500

    destroy all in seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+Movie.destroy_all
+
 users = [
     {
       "name": "Patty"
