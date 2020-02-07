3a0dd871445c958195c83462546bc861a5c61f1e
github username <github email address>
2020-02-07 15:03:11 -0500

    models and controller

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  get 'photos/index'
+  get 'photos/show'
+  get 'photos/create'
+  get 'photos/update'
+  get 'photos/destroy'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
