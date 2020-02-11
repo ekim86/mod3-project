22f83118c99aa82216437736208364e8d3b8b128
github username <github email address>
2020-02-11 10:53:18 -0500

    up to date

--- frontend/index.js
+++ frontend/index.js
@@ -1,16 +1,13 @@
 window.addEventListener('DOMContentLoaded', (event) => {
   console.log('Photo App');
-
   const photoList = document.getElementById('photo-list');
   const photoDetail = document.getElementById('photo-detail');
   const photoUrl = 'http://localhost:3000/api/v1/photos';
   // `${photoUrl}/${photo.id}`
   // make sure to cd into backend to run rails s
-
   fetchPhoto();
   displayPhotoDetails();
   detailBtns();
-
   function fetchPhoto() {
     fetch(`${photoUrl}`)
       .then((response) => {
@@ -23,8 +20,6 @@ window.addEventListener('DOMContentLoaded', (event) => {
           renderPhoto(photo));
       }); //closes fetch
   } // closes function fetchphotos
-
-
   function renderPhoto(photo) {
     const photoDiv = document.createElement('div');
     photoDiv.className = 'photo-thumbnail';
@@ -34,7 +29,6 @@ window.addEventListener('DOMContentLoaded', (event) => {
     <br><br>`;
     photoList.appendChild(photoDiv);
   } // closes function renderphoto
-
   function showDetails(photo) {
    console.log(photo);
    photoDetail.dataset.id = photo.id;
@@ -44,17 +38,13 @@ window.addEventListener('DOMContentLoaded', (event) => {
       <img src="${photo.img_url}"/>
       <br>
       <button class='like-btn'>Likes: ${photo.likes}</button>
-
       <br>
       <br>
       Description:<textarea>${photo.description}</textarea><br><Br>
       <button class='save-btn'>Save Description</button><br>
       <button class='delete-btn'>Delete</button>
     `;
-
   } //end of show details 
-
-
   function fetchPhotoDetails(id) {
     fetch(`${photoUrl}/${id}`)
         .then(resp => resp.json())
@@ -62,8 +52,6 @@ window.addEventListener('DOMContentLoaded', (event) => {
           showDetails(photo);
         });
   }
-
-
 function displayPhotoDetails() {
   photoList.addEventListener('click', function (event) {
     if (event.target.className === 'photo-thumbnail') {
@@ -72,14 +60,11 @@ function displayPhotoDetails() {
     }
   }); //closes event listener
 } //end of disphotodetails 
-
-
 function detailBtns() {
   photoDetail.addEventListener('click', function(event) {
     let id = photoDetail.dataset.id;
     // can get the id without having to always do event.target
     if (event.target.className === 'like-btn') {
-
       let likes = event.target.innerText.split(' ')[1];
       // we got the number that isn't yet a number
       let numLikes = parseInt(likes) + 1;
@@ -92,7 +77,6 @@ function detailBtns() {
       // let likes = parseInt(event.target.innerText) + 1
       // event.target.innerText = likes
       // when it's like this => <button class='like-btn'>${photo.likes}</button>
-
       fetch(`${photoUrl}/${id}`, {
         method: "PATCH",
         headers: {
@@ -100,13 +84,11 @@ function detailBtns() {
           accept: "application/json"
         },
         body: JSON.stringify({likes: numLikes})
-      });
-
+      })
     }
     if (event.target.className === 'save-btn') {
       let desc = document.querySelector('textarea').value
       // console.log(desc, "description") WORKS!
-
       fetch(`${photoUrl}/${id}`, {
         method: "PATCH",
         headers: {
@@ -117,7 +99,6 @@ function detailBtns() {
       })
     }
     if (event.target.className === 'delete-btn') {
-
       photoDetail.remove()
       let photoThumbnails = document.getElementsByClassName('photo-thumbnail')
       Array.from(photoThumbnails).forEach(photo => {
@@ -125,20 +106,14 @@ function detailBtns() {
         photo.remove()
       }
       })
-  
+      fetch(`${photoUrl}/${id}`, {
+        method: "DELETE",
+        headers: {
+          "content-type": "application/json",
+          accept: "application/json"
+        }
+      });
     }
-
-    fetch(`${photoUrl}/${id}`, {
-      method: "DELETE",
-      headers: {
-        "content-type": "application/json",
-        accept: "application/json"
-      }
-    });
-  } // closes if
-
   });  //end of listener
 } //end of detailbtns
-
-
 });
